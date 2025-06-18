3d31479cab82c6e169dd08769963027b95138a74
Matej Almasi <almasi.matej@protonmail.com>
2025-06-18 19:21:46 +0200

    reorganize and cleanup the code

--- src/extension.ts
+++ src/extension.ts
@@ -7,26 +7,30 @@ const minConfigName = "minSaves";
 const maxConfigName = "maxSaves";
 
 export function activate(context: vscode.ExtensionContext) {
-  let tips = loadTips(context);
-
   let nextTipCountdown = getNextCountdown();
 
+  // Show a Tip on demand
+  const disposable = vscode.commands.registerCommand(
+    "pragmatic-toaster.pragmaticTip",
+    () => {
+      showPragmaticTip(context);
+      nextTipCountdown = getNextCountdown();
+    }
+  );
+
+  context.subscriptions.push(disposable);
+
+  // Show a tip every few saves
   vscode.workspace.onDidSaveTextDocument(() => {
     --nextTipCountdown;
 
     if (nextTipCountdown === 0) {
-      try {
-        tips = loadTips(context);
-      } catch (err) {
-        showFailedLoadingMessage(err);
-        return;
-      }
-
-      showPragmaticTip(tips);
+      showPragmaticTip(context);
       nextTipCountdown = getNextCountdown();
     }
   });
 
+  // Update countdown when the settings change
   vscode.workspace.onDidChangeConfiguration((event) => {
     if (
       event.affectsConfiguration(`${extensionName}.${minConfigName}`) ||
@@ -35,26 +39,67 @@ export function activate(context: vscode.ExtensionContext) {
       nextTipCountdown = getNextCountdown();
     }
   });
-
-  const disposable = vscode.commands.registerCommand(
-    "pragmatic-toaster.pragmaticTip",
-    () => {
-      try {
-        tips = loadTips(context);
-      } catch (err) {
-        showFailedLoadingMessage(err);
-        return;
-      }
-
-      showPragmaticTip(tips);
-      nextTipCountdown = getNextCountdown();
-    }
-  );
-
-  context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+function getNextCountdown(): number {
+  const config = vscode.workspace.getConfiguration(extensionName);
+  let min = config.get<number>(minConfigName, 5);
+  const max = config.get<number>(maxConfigName, 10);
+
+  // having min === 0 and max > 0 could lead to randomly setting
+  // the countdown to 0 which would lead to no longer showing any
+  // tips on file save
+  if (min === 0 && max > 0) {
+    min = 1;
+  }
+
+  if (max < min) {
+    vscode.window.showWarningMessage(
+      `Pragmatic Toaster ${maxConfigName} is lower than ${minConfigName}.\
+       No toasts for today, until you fix the settings!`
+    );
+    return -1;
+  }
+
+  return getRandomFromRange(min, max);
+}
+
+function getRandomFromRange(lower: number, upper: number) {
+  const range = upper - lower;
+  const offset = lower;
+  return Math.round(Math.random() * range + offset);
+}
+
+function showPragmaticTip(context: vscode.ExtensionContext) {
+  let tips;
+
+  try {
+    tips = loadTips(context);
+  } catch (err) {
+    // should never happen as we control the extension files,
+    // but one can never be sure...
+    showFailedLoadingMessage(err);
+    return;
+  }
+
+  const tip = tips.pop();
+  vscode.window.showInformationMessage(
+    `${tip?.title}: ${tip?.extra} ${tip?.number}.`,
+    "OK"
+  );
+}
+
+function showFailedLoadingMessage(err: unknown) {
+  const message = "Failed loading Pragmatic Tips";
+  console.error(`${message}: ${err}`);
+  vscode.window.showErrorMessage(message);
+}
+
+interface Tip {
+  title: string;
+  extra: string;
+  number: string;
+}
 
 function loadTips(context: vscode.ExtensionContext): Tip[] {
   const tipsPath = path.join(context.extensionPath, "resources", "tips.json");
@@ -69,12 +114,6 @@ function loadTips(context: vscode.ExtensionContext): Tip[] {
   return randomPermutation(parsed.length).map((i) => parsed[i]);
 }
 
-interface Tip {
-  title: string;
-  extra: string;
-  number: string;
-}
-
 function randomPermutation(n: number) {
   const arr = Array.from({ length: n }, (_, i) => i);
 
@@ -84,40 +123,4 @@ function randomPermutation(n: number) {
   }
 
   return arr;
-}
-
-function getNextCountdown(): number {
-  const config = vscode.workspace.getConfiguration(extensionName);
-  const min = config.get<number>(minConfigName, 5);
-  const max = config.get<number>(maxConfigName, 10);
-
-  if (max < min) {
-    vscode.window.showWarningMessage(
-      `Pragmatic Toaster ${maxConfigName} is lower than ${minConfigName}.\
-     No toasts for today, until you fix the settings!`
-    );
-    return -1;
-  }
-
-  return getRandomFromRange(min, max);
-}
-
-function getRandomFromRange(lower: number, upper: number) {
-  const range = upper - lower;
-  const offset = lower;
-  return Math.round(Math.random() * range + offset);
-}
-
-function showFailedLoadingMessage(err: unknown) {
-  const message = "Failed loading Pragmatic Tips";
-  console.error(`${message}: ${err}`);
-  vscode.window.showErrorMessage(message);
-}
-
-function showPragmaticTip(tips: Tip[]) {
-  const tip = tips.pop();
-  vscode.window.showInformationMessage(
-    `${tip?.title}: ${tip?.extra} ${tip?.number}.`,
-    "OK"
-  );
 }
